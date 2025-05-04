ad6f1614f6d805c0c1f337dcd5d727d3cf68403c
Inplex-sys <69421356+Inplex-sys@users.noreply.github.com>
2025-05-04 13:10:16 +0200

    feat: add .env.example and update package.json, bun.lock, and hardhat.config.ts for Bun integration

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,8 +1,18 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+if (!process.env.PRIVATE_KEY) {
+    throw new Error("Please set your PRIVATE_KEY in a .env file");
+}
+
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+    solidity: "0.8.28",
+    networks: {
+        optimism: {
+            url: "https://optimism.meowrpc.com",
+            accounts: [Bun.env.PRIVATE_KEY],
+        },
+    },
 };
 
 export default config;
